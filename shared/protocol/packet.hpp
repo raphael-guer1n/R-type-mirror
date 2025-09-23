8745de5c6da87261d5b025190d0dcae3e5541597
Maryse Katary <maryse.katary@epitech.eu>
2025-09-23 17:56:23 +0200

    feat(protocol): define core packet structures and generic serializer for binary protocol

--- shared/protocol/packet.hpp
+++ shared/protocol/packet.hpp
@@ -8,35 +8,71 @@
 #pragma once
 #include <cstdint>
 
-enum class PacketType : uint16_t {
-    CONNECT_REQ = 1,
-    CONNECT_ACK = 2,
-    INPUT       = 3,
-    SNAPSHOT    = 4,
-    EVENT       = 5,
-    PING        = 6,
-    PONG        = 7
-};
-
 #pragma pack(push, 1)
-struct PacketHeader {
-    uint16_t type;
-    uint16_t size;
-    uint32_t seq;
-};
+
+namespace protocol {
+
+    enum class PacketType : uint16_t {
+        CONNECT_REQ = 1,
+        CONNECT_ACK = 2,
+        INPUT       = 3,
+        SNAPSHOT    = 4,
+        EVENT       = 5,
+        PING        = 6,
+        PONG        = 7
+    };
+
+    struct PacketHeader {
+        uint16_t type;
+        uint16_t size;
+        uint32_t seq;
+    };
+
+    struct ConnectReq {
+        uint32_t clientId;
+    };
+
+    struct ConnectAck {
+        uint32_t playerEntityId;
+        uint32_t tickRate;
+    };
+
+    struct InputPacket {
+        uint32_t clientId;
+        uint32_t tick;
+        uint16_t keys;
+    };
+
+    struct EntityState {
+        uint32_t entityId;
+        float    x;
+        float    y;
+        float    vx;
+        float    vy;
+        uint8_t  type;
+        uint8_t  hp;
+    };
+
+    struct Snapshot {
+        uint32_t tick;
+        uint16_t entityCount;
+        EntityState entities[];
+    };
+
+    struct EventPacket {
+        uint32_t tick;
+        uint16_t eventType;
+        uint32_t entityId;
+    };
+
+    struct PingPacket {
+        uint64_t timestamp;
+    };
+
+    struct PongPacket {
+        uint64_t timestamp;
+    };
+
+}
+
 #pragma pack(pop)
-
-struct ConnectReq {
-    uint32_t clientId;
-};
-
-struct ConnectAck {
-    uint32_t serverId;
-    uint32_t tickRate;
-};
-
-struct InputPacket {
-    uint32_t clientId;
-    uint32_t tick;
-    uint8_t keys;
-};
--- shared/protocol/serializer.hpp
+++ shared/protocol/serializer.hpp
@@ -2,23 +2,40 @@
 ** EPITECH PROJECT, 2025
 ** R-type-mirror
 ** File description:
-** Serializer
+** serializer
 */
 
 #pragma once
 #include <vector>
+#include <cstdint>
 #include <cstring>
+#include <type_traits>
+#include <stdexcept>
 
-template<typename T>
-std::vector<uint8_t> serialize(const T& data) {
-    std::vector<uint8_t> buffer(sizeof(T));
-    std::memcpy(buffer.data(), &data, sizeof(T));
-    return buffer;
-}
+namespace protocol {
 
-template<typename T>
-T deserialize(const std::vector<uint8_t>& buffer) {
-    T data;
-    std::memcpy(&data, buffer.data(), sizeof(T));
-    return data;
+    template<typename T>
+    std::vector<uint8_t> serialize(const T& data) {
+        static_assert(std::is_trivially_copyable_v<T>, 
+                    "serialize<T> requires trivially copyable type");
+
+        std::vector<uint8_t> buffer(sizeof(T));
+        std::memcpy(buffer.data(), &data, sizeof(T));
+        return buffer;
+    }
+
+    template<typename T>
+    T deserialize(const std::vector<uint8_t>& buffer, std::size_t offset = 0) {
+        static_assert(std::is_trivially_copyable_v<T>, 
+                    "deserialize<T> requires trivially copyable type");
+
+        if (buffer.size() < offset + sizeof(T)) {
+            throw std::runtime_error("deserialize<T>: buffer too small");
+        }
+
+        T obj;
+        std::memcpy(&obj, buffer.data() + offset, sizeof(T));
+        return obj;
+    }
+
 }
