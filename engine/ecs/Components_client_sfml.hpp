045e10162aac8e67c768832dc57f6523f7ddd719
Alexis <alexis.constantinopoulos@epitech.eu>
2025-09-23 15:49:20 +0200

    feat: add drawable component for SFML graphics

---
+++ engine/ecs/Components_client_sfml.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/System/Vector2.hpp>
+
+namespace component
+{
+    struct drawable
+    {
+        sf::Vector2f size{30.f, 30.f};
+        sf::Color color{sf::Color::White};
+        drawable() = default;
+        drawable(sf::Vector2f s, sf::Color c) : size(s), color(c) {}
+    };
+}
