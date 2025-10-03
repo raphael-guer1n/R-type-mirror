dccd16ed8d59f3a71999aa8e3fb3f79f1bfbf969
lisaglmt <lisa.goulmot@epitech.eu>
2025-10-03 20:49:40 +0200

    feat: background segfault fix

--- src/client/Background.cpp
+++ src/client/Background.cpp
@@ -7,7 +7,6 @@ R_Type::Background::Background(R_Type::Rtype& rtype)
 {
     R_Graphic::intVec2 pos = rtype.getApp().getWindow().getSize();
     auto& registry = rtype.getRegistry();
-    registry.register_component<component::background_tag>();
     auto e = registry.spawn_entity();
 
     registry.add_component(e, component::position{0.0f, 0.0f});
@@ -20,11 +19,11 @@ R_Type::Background::Background(R_Type::Rtype& rtype)
         rtype.getApp().getWindow().getSize()
     );
     R_Graphic::textureRect rect(0, 0,
-    rtype.getApp().getWindow().getSize().x,
-    rtype.getApp().getWindow().getSize().y
+        rtype.getApp().getWindow().getSize().x,
+        rtype.getApp().getWindow().getSize().y
     );
     registry.emplace_component<component::drawable>(e, tex, rect);
-
+    
     auto e1 = registry.spawn_entity();
     registry.add_component(e1, component::position{static_cast<float>(pos.x), 0.0f});
     registry.add_component(e1, component::velocity{-100.0f, 0.0f});
@@ -36,10 +35,19 @@ R_Type::Background::Background(R_Type::Rtype& rtype)
         rtype.getApp().getWindow().getSize()
     );
     R_Graphic::textureRect rect1(0, 0,
-    rtype.getApp().getWindow().getSize().x,
-    rtype.getApp().getWindow().getSize().y
+        rtype.getApp().getWindow().getSize().x,
+        rtype.getApp().getWindow().getSize().y
     );
     registry.emplace_component<component::drawable>(e1, tex1, rect1);
+    
+    std::cout << "Entity e=" << e 
+          << " pos=" << registry.get_components<component::position>()[e].has_value()
+          << " bg="  << registry.get_components<component::background_tag>()[e].has_value()
+          << std::endl;
+std::cout << "Entity e1=" << e1 
+          << " pos=" << registry.get_components<component::position>()[e1].has_value()
+          << " bg="  << registry.get_components<component::background_tag>()[e1].has_value()
+          << std::endl;
 }
 
 void R_Type::scroll_reset_system(engine::registry& r,
@@ -54,6 +62,11 @@ void R_Type::scroll_reset_system(engine::registry& r,
         if (backgrounds[i].has_value() && positions[i].has_value()) {
             bg_positions.push_back(&positions[i].value());
         }
+    }
+    if (bg_positions.size() < 2) {
+    std::cerr << "Error: not enough background entities (" 
+              << bg_positions.size() << " found)\n";
+    return;
     }
     auto& bg1 = *bg_positions[0];
     auto& bg2 = *bg_positions[1];
