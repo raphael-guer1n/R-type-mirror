39679a311b04de05df5bae92b120912c81b7d0a1
lisaglmt <lisa.goulmot@epitech.eu>
2025-11-02 21:51:52 +0100

    feat: add hudUpdate for score and health

--- src/client/Hud.hpp
+++ src/client/Hud.hpp
@@ -33,11 +33,12 @@ namespace R_Type
     public:
         Hud(R_Type::Rtype &rtype);
         ~Hud() = default;
+        void hudUpdate(R_Type::Rtype &rtype);
         void setChargeLevel(R_Type::Rtype &rtype, float level);
         void drawOverlay(R_Type::Rtype &rtype);
         void startLevelAnimation(int level, engine::registry &registry);
         void drawText(std::string &text, float hudScale, float x, float y,
-            R_Type::Rtype &rtype, bool level = false);
+            R_Type::Rtype &rtype, bool level = false, bool score = false);
         void setHealth(std::uint8_t value) {_health = static_cast<int>(value);};
         void setScore(uint32_t value) {_score = static_cast<int>(value);};
 
